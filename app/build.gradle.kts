c8b9651f870c264db562ba5f1b375987f193f7df
Arty Fact <chris.bush+arty-fact@mongodb.com>
2023-01-18 17:49:20 +0000

    Automatic update Initiated by https://github.com/mongodb-university/realm-template-apps/commit/9b0230ac7ad28ff4ef7c400e019a934cfab39bc3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("io.realm.kotlin")
+    id("io.realm.kotlin") version "1.4.0"
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.mongodb.app"
         minSdk = 28
-        targetSdk = 31
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
@@ -18,12 +18,30 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
-    implementation("io.realm.kotlin:library-sync:1.0.0") // DON'T FORGET TO UPDATE VERSION IN PROJECT GRADLE
+    implementation("androidx.compose.ui:ui:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.2")
+    implementation("androidx.compose.foundation:foundation:1.3.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.3.2")
+
+    implementation("androidx.compose.material3:material3:1.0.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("io.realm.kotlin:library-sync:1.4.0") // DON'T FORGET TO UPDATE VERSION IN PROJECT GRADLE
 }
